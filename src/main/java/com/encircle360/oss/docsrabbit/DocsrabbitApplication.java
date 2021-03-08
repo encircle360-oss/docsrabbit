e6b3a9d1723aec95fae4139ba2f383fe19f316d1
peter <ps@web-eifel.de>
2021-03-08 10:56:05 +0100

    added basic document handling

--- src/main/java/com/encircle360/oss/docsrabbit/DocsrabbitApplication.java
+++ src/main/java/com/encircle360/oss/docsrabbit/DocsrabbitApplication.java
@@ -1,7 +1,12 @@
 package com.encircle360.oss.docsrabbit;
 
+import static freemarker.template.Configuration.VERSION_2_3_28;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.encircle360.oss.docsrabbit.wrapper.JsonNodeObjectWrapper;
 
 @SpringBootApplication
 public class DocsrabbitApplication {
@@ -10,4 +15,8 @@ public class DocsrabbitApplication {
         SpringApplication.run(DocsrabbitApplication.class, args);
     }
 
+    @Bean
+    JsonNodeObjectWrapper jsonNodeObjectWrapper() {
+        return new JsonNodeObjectWrapper(VERSION_2_3_28);
+    }
 }
---
+++ src/main/java/com/encircle360/oss/docsrabbit/wrapper/model/JsonNullNodeModel.java
@@ -0,0 +1,7 @@
+package com.encircle360.oss.docsrabbit.wrapper.model;
+
+import freemarker.ext.util.ModelFactory;
+
+public class JsonNullNodeModel {
+    static public final ModelFactory FACTORY = (object, wrapper) -> null;
+}
