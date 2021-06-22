5010f3c9fc2a2519fedccbcb3880c7106c35f686
peter <ps@web-eifel.de>
2021-06-22 11:28:59 +0200

    added missing api doc

--- src/main/java/com/encircle360/oss/docsrabbit/dto/convert/ConverterResultDTO.java
+++ src/main/java/com/encircle360/oss/docsrabbit/dto/convert/ConverterResultDTO.java
@@ -1,5 +1,6 @@
 package com.encircle360.oss.docsrabbit.dto.convert;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "ConverterResult", description = "Result of a document conversion")
 public class ConverterResultDTO {
 
+    @Schema(name = "format", description = "The file format of the base64", example = "pdf")
     private String format;
 
+    @Schema(name = "base64", description = "Base64 encoded data of the converted file.")
     private String base64;
 }
